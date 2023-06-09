6ff94c6b5ae98e26bd1560e3fe4eee43c975d1b2
seon1112 <130538529+seon1112@users.noreply.github.com>
2023-06-09 14:20:43 +0900

    [Silver V] Title: 영화감독 숌, Time: 388 ms, Memory: 88616 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    	Scanner sc=new Scanner(System.in);
+    	int n=sc.nextInt();
+    	int count=1;
+    	int num=666;
+    	
+    	while(count!=n) {
+    		num++;
+    		if((num+"").contains("666")) {
+    			count++;
+    		}
+    	}
+    	System.out.println(num);
+    }
+}
